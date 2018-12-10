58d030103cce01915e0666872608e49a8ad0df19
N3lox <dorian24130@gmail.com>
2018-12-10 12:51:32 +0100

    Kitchen Action

--- Shared/Model/IKitchenAction.cs
+++ Shared/Model/IKitchenAction.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Shared.Model
+namespace Shared
 {
-    interface IKitchenAction
+    public static class KitchenActionFactory
     {
+
+        public static IAction CreateKitchenAction_(string Name = "Kitchen")
+        {
+            switch (Name)
+            {
+                case "Prepare":
+                    return new KitchenAction_("Prepare", 1);
+
+                case "Peel":
+                    return new KitchenAction_("Peel", 1);
+
+                case "BigWash":
+                    return new KitchenAction_("BigWash", 2);
+
+
+            }
+        }
+
     }
 }
