e9d98cc61491e4cdcb47eb6e7f5902db829661be
Mehdi SANSAL <xp38badi@gmail.com>
2018-12-10 14:53:15 +0100

    Interface with winforms and test class

---
+++ FormView.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class FormView : Form
+    {
+       //Declaring variables
+        private int xPosWaiter;
+        private int yPosWaiter;
+        private int xClient;
+        private int yClient;
+        private int xButler;
+        private int yButler;
+
+        public FormView()
+        {
+            InitializeComponent();
+
+            // initial positions
+
+            xPosWaiter = 540;
+            yPosWaiter = 95;
+
+            xClient = 10;
+            yClient = 10;
+
+            xButler = 880;
+            yButler= 625;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormView_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormView_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+           
+          //Drowing actors
+
+           e.Graphics.FillRectangle(Brushes.Blue, xPosWaiter, yPosWaiter, 10, 10);
+           e.Graphics.FillRectangle(Brushes.Black, xClient, yClient, 10, 10);
+           e.Graphics.FillRectangle(Brushes.Brown, xButler, yButler, 10, 10);
+
+        }
+
+        private void FormView_Load(object sender, EventArgs e)
+        {
+
+        }
+        // timer
+        private void ticks_Tick(object sender, EventArgs e)
+        {
+            //Translating positions
+            //test
+            xPosWaiter += 2;
+            Invalidate();
+        }
+
+
+    }
+
+}
--- WindowsFormsAppTests/Properties/AssemblyInfo.cs
+++ WindowsFormsAppTests/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Les informations générales relatives à un assembly dépendent de
+// Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("RoomView")]
+[assembly: AssemblyTitle("WindowsFormsAppTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RoomView")]
+[assembly: AssemblyProduct("WindowsFormsAppTests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
-// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
+// aux composants COM.  Si vous devez accéder à un type dans cet assembly à partir de 
 // COM, affectez la valeur true à l'attribut ComVisible sur ce type.
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("e2b87df2-e0c3-4bee-96aa-83b31682d702")]
+[assembly: Guid("934bd980-cff4-486c-ae8d-8fd21b1359ac")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
 //      Version principale
-//      Version secondaire
+//      Version secondaire 
 //      Numéro de build
 //      Révision
 //
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// Vous pouvez spécifier toutes les valeurs ou vous pouvez définir par défaut les numéros de build et de révision 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
