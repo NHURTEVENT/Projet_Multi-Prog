14d24b3ea235ae666f4006ef282369069ca67c4a
N3lox <dorian24130@gmail.com>
2018-12-10 12:33:11 +0100

    Kitchen Personnel Factory
    Action

--- KitchenModel/KitchenPersonnelFactory.cs
+++ KitchenModel/KitchenPersonnelFactory.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shared;
 
 namespace Model
 {
-    public class KitchenPersonnelFactory
+    public static class KitchenPersonnelFactory
     {
+
+        public static IKitchenPartyChef CreatePrepareDish(string Name)
+        {
+            return new PartyChef(KitchenActionFactory.CreateKitchenAction_("Prepare"), Name);
+        }
+
+        public static IKitchenPartyChef CreateDish(string Name)
+        {
+            return new PartyChef(KitchenActionFactory.CreateKitchenAction_(), Name);
+        }
+
     }
 }
